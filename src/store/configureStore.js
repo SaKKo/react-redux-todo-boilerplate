12ad33f1d85c683559b4a44d80bb48a515288c91
sakko <saklism@gmail.com>
2016-02-22 13:45:18 +0700

    hot reload

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -13,13 +13,13 @@ export default function configureStore(initialState) {
     store = createStore(rootReducer, initialState);
   }
 
-  // if (module.hot) {
-  //   // Enable Webpack hot module replacement for reducers
-  //   module.hot.accept('../reducers', () => {
-  //     const nextReducer = require('../reducers');
-  //     store.replaceReducer(nextReducer);
-  //   });
-  // }
+  if (module.hot) {
+    // Enable Webpack hot module replacement for reducers
+    module.hot.accept('../reducers', () => {
+      const nextReducer = require('../reducers');
+      store.replaceReducer(nextReducer);
+    });
+  }
 
   return store;
 }
